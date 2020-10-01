49230ba7cc04db192cb0df2092bcedd2e045a805
Sumit Jamgade <sumitjami@gmail.com>
2020-10-01 11:26:02 +0200

    mod vga
    lots of stuff to getof the ground

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,20 @@
-
-
 #![no_std]
 #![no_main]
 
+extern crate rlibc;
+mod vga_buffer;
 use core::panic::PanicInfo;
-
-static HELLO: &[u8] = b"Hello World !!";
 
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
-
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate( ) {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-    loop {}
+    println!("Hello through macro {}", "!");
+    panic!("Dang it !!!");
 }
+
