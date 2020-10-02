6ee8f5702055cbb77b7d1ab1ac690b5c29101797
sum12 <sumitjami@gmail.com>
2020-10-02 15:13:51 +0200

    independent bianries for integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,15 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(osrs::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-extern crate rlibc;
 use core::panic::PanicInfo;
-
-mod vga_buffer;
-mod serial;
-
-
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
+use osrs::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello through macro {}", "!");
+    println!("hello through macro {} in main.rs", "!");
 
     #[cfg(test)]
     test_main();
@@ -38,48 +17,14 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T 
-where 
-    T: Fn(),
-{
-    fn run(&self)-> () {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]")
-    }
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    osrs::serial_println!("{}", info);
+    loop {}
 }
 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run()
-    }
-    exit_qemu(QemuExitCode::Success);
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    osrs::test_panic_handler(info)
 }
-
-
-#[test_case]
-fn trivial_assert() {
-    assert_eq!(0,0);
-}
-
---
+++ tests/basic_boot.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(osrs::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo)-> !{
+    osrs::test_panic_handler(info);
+}
+
+use osrs::println;
+
+#[test_case]
+fn test_println(){
+    println!("test_println output");
+}
